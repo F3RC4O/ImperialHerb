43d76b04816420eb5c445aebe417f4ced335a53a
f3rc4o <f3rc4o@gmail.com>
2021-02-17 00:16:30 -0600

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    $('#exampleModalCenter').modal('show');
+});
+
+
+ $('.carousel-main').owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+})
