9a9d41bb127e61fb8ac4db670ccc9071a782af47
f3rc4o <f3rc4o@gmail.com>
2021-02-20 01:29:42 -0600

    Diseño más parecido

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,17 @@
-$( document ).ready(function() {
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 100) {
+        $("nav").removeClass("bg-imperial-menu").addClass("bg-imperialscroll");
+    } else {
+        $("nav").removeClass("bg-imperialscroll").addClass("bg-imperial-menu");
+    }
+});
+
+$(document).ready(function () {
     $('#exampleModalCenter').modal('show');
 });
 
 
- $('.carousel-main').owlCarousel({
+$('.carousel-main').owlCarousel({
     items: 3,
     loop: true,
     autoplay: true,
@@ -11,21 +19,21 @@ $( document ).ready(function() {
     margin: 10,
     nav: true,
     dots: false,
-    navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    navText: ['<span class="fas fa-chevron-left fa-2x"></span>', '<span class="fas fa-chevron-right fa-2x"></span>'],
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
         },
-        600:{
-            items:2,
-            nav:false
+        600: {
+            items: 2,
+            nav: false
         },
-        1000:{
-            items:3,
-            nav:true,
-            loop:true
+        1000: {
+            items: 3,
+            nav: true,
+            loop: true
         }
     }
 })
